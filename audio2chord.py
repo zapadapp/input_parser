8adf453402a0b55da1c0d9f95de3de1e890d2e0c
JuanMa <jmartinmora5@gmail.com>
2022-07-23 12:10:02 -0300

    adding chord recognition and notes from chord

--- audio2chord.py
+++ audio2chord.py
@@ -21,33 +21,12 @@ WORKSPACE = os.path.dirname(FILE_PATH)
 sys.path.insert(0, os.path.join(WORKSPACE, "chord_cnn"))
 from parser_data import normalizeShape, correctShape
 
-
-CATEGORIES = [
-    "C4",
-    "C4",
-    "Mayor-DOs",
-    "Mayor-FA",
-    "Mayor-LA",
-    "Mayor-MI",
-    "Mayor-MIb",
-    "Mayor-RE",
-    "Mayor-SI",
-    "Mayor-SIb",
-    "Mayor-SOL",
-    "Mayor-SOLs",
-    "Menor-DO",
-    "Menor-DOs",
-    "Menor-FA",
-    "Menor-FAs",
-    "Menor-LA",
-    "Menor-MI",
-    "Menor-MIb",
-    "Menor-RE",
-    "Menor-SI",
-    "Menor-SIb",
-    "Menor-SOL",
-    "Menor-SOLs"
-]
+CATEGORIES = ["A#3","A#4","A#5","A3","A4","A5",
+              "B3", "B4","B5","C#3","C#4","C#5",
+              "C3","C4","C5","D#3","D#4", "D#5",
+              "D3","D4","D5","E3","E4", "E5",
+              "F#3","F#4", "F#5","F3","F4","F5",
+              "G#3","G#4","G#5","G3","G4","G5",]
 
 chord_model = keras.models.load_model('/home/maxo/workspace/chord_cnn/modelo-acordes-v01.h5')
 
@@ -59,6 +38,72 @@ def filterLowSamples(samples):
     # remove elements for given indexes
     return np.delete(samples, indexes)
 
+def correctShape(chroma_shape):
+    return chroma_shape == 130
+
+def normalizeShape(chroma_mat):
+    nums = 0
+    #init_shape tiene la dimension de columnas. 
+    init_shape= chroma_mat.shape[1]
+    #Me fijo cuantas columnas faltan por rellenar
+    nums = 130 - init_shape
+    #itero nums copiando el anterior
+    arreglo = np.array(chroma_mat[:,init_shape-1])
+   
+    i = 0
+    while i < nums :
+        chroma_mat= np.column_stack((chroma_mat,arreglo))  
+        i = i +1 
+    return chroma_mat
+
+
+def getChordFromRNN(file_path, sample_rate):
+    my_model = keras.models.load_model('modelo-acordes-v01.h5')
+
+    signal, sr = librosa.load(file_path, sr=sample_rate)
+
+    # extract Chroma
+    chroma = librosa.feature.chroma_cens(y=signal, sr=sr)
+
+    if not correctShape(chroma.shape[1]) :
+        chroma = normalizeShape(chroma)
+
+    chroma_reshape = tf.reshape(chroma, [ 1,12,130])
+    my_prediction = my_model.predict(chroma_reshape)
+   
+    index = np.argmax(my_prediction)
+    print("chord: " + CATEGORIES[index])
+
+    return CATEGORIES[index]
+
+def getNotesFromChords(chord):
+    notas_string = ['C','C#','D','D#','E','F','F#','G','G#','A','A#','B']	
+    nota_2 =''
+    nota_3 =''
+    octaveIndex = 2
+    secondNoteIndex = 3
+
+    if len(chord) == 2:
+        octaveIndex = 1
+        secondNoteIndex = 4
+	
+    nota = chord[0]
+    octava = int(chord[octaveIndex])
+    indice = notas_string.index(nota)
+    if ((indice + secondNoteIndex) / 12) >= 1 :
+        print(indice + secondNoteIndex)
+        nota_2 = notas_string[((indice + secondNoteIndex )%12)] + str(octava + 1)
+    else:
+        nota_2 = notas_string[(indice + secondNoteIndex)]  + str(octava)
+    if ((indice + 7) / 12) >= 1 :
+        nota_3 = notas_string[((indice + 7) %12)] + str(octava + 1)
+    else:
+        nota_3 = notas_string[(indice + 7)]  + str(octava)
+
+    triada = [chord, nota_2, nota_3]
+     	 	
+    return triada
+
 def detectAndPrintChord(s,q, y, sr, samples, a, b, scorePath):
     # limit the audio input from sample in index a to sample in index b, unless b is 999 which means that it is the end of the audio data
     if b == 999:
@@ -68,19 +113,20 @@ def detectAndPrintChord(s,q, y, sr, samples, a, b, scorePath):
     
     # extract Chroma
     # extract Chroma
-    chroma = librosa.feature.chroma_cens(y=data, sr=sr)
-    if not correctShape(chroma.shape[1]) :
-        chroma = normalizeShape(chroma)
+    #chroma = librosa.feature.chroma_cens(y=data, sr=sr)
+    #if not correctShape(chroma.shape[1]) :
+    #    chroma = normalizeShape(chroma)
     
-    chroma_reshape = tf.reshape(chroma, [ 1,12,130])
-    my_prediction = chord_model.predict(chroma_reshape)
-    print(my_prediction)
-    index = np.argmax(my_prediction)
-    print("chord: " + CATEGORIES[index])
-    playedChord = CATEGORIES[index]
+    #chroma_reshape = tf.reshape(chroma, [ 1,12,130])
+    #my_prediction = chord_model.predict(chroma_reshape)
+    #print(my_prediction)
+    #index = np.argmax(my_prediction)
+    playedChord = getChordFromRNN(y=data,sr=sr)
+    print("chord: " + playedChord)
+    
     q.put(playedChord)
-    if playedChord == "C4":
-        s.append(chord.Chord(['C4','E4','G4']))
+  
+    s.append(chord.Chord(getNotesFromChords(playedChord)))
     print(s.write('lily.png', fp=os.path.join("tmp", scorePath)))
     #q.put(s)
     #print("Detected note: {}".format(str(librosa.hz_to_note(f[peak_i[0]]))))
